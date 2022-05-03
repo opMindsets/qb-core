35b3c0335fc1659f62255453ccd4ea3c0ad23fba
pikax0 <jonnie_fagundes1@hotmail.com>
2022-05-03 17:46:10 +0200

    add BS job

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -279,4 +279,31 @@ QBShared.Jobs = {
             },
         },
 	},
+    ["burgershot"] = {
+		label = "Burgershot Employee",
+		defaultDuty = true,
+		grades = {
+            ['0'] = {
+                name = "Trainee",
+                payment = 50
+            },
+			['1'] = {
+                name = "Employee",
+                payment = 75
+            },
+			['2'] = {
+                name = "Burger Flipper",
+                payment = 100
+            },
+			['3'] = {
+                name = "Manager",
+                payment = 125
+            },
+			['4'] = {
+                name = "CEO",
+				isboss = true,
+                payment = 150
+            },
+        },
+	},
 }
